bb1edc5b05c149dd6911a537fdcbce498ed06043
Giovanna <giovannacaron.tds1.senai@gmail.com>
2025-04-30 20:20:02 -0300

    Feat: create noticas banner

--- app/noticias/page.jsx
+++ app/noticias/page.jsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 import Loader from "../../components/Loader";
+import Header from "../../components/Header";
 import styles from "../../styles/Noticias.module.css";
 
 export default function Noticias() {
@@ -21,6 +22,16 @@ export default function Noticias() {
 
   return (
     <div className={styles.Container}>
+    <Header bannerTitle={"BORA VIAJAR"} />
+
+    <div className={styles.bannerContainer}>
+        <div className={styles.textContainer}>
+        <h2 className={styles.titleBanner}> Planeje os melhores momentos</h2>
+        <h1 className={styles.subtitleBanner}>Bora viajar!</h1>
+        </div>
+        <button className={styles.buttonBanner}>Comece Já!</button>
+    </div>
+
       <div className={styles.destinoSection}>
         <div className={styles.DestinoContainer}>
             <div className={styles.ContentContainer}>
