1eb968d6bcc1802eb2fc11c7794a83aac2778232
Giovanna <giovannacaron.tds1.senai@gmail.com>
2025-05-04 09:11:57 -0300

    feat: create content noticias pagr

--- app/noticias/page.jsx
+++ app/noticias/page.jsx
@@ -1,11 +1,13 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
+import Image from 'next/image';
 import Loader from "../../components/Loader";
 import Header from "../../components/Header";
+import NoticiaCard from "../../components/NoticiaCard";
 import styles from "../../styles/Noticias.module.css";
 
-export default function Noticias() {
+export default function Noticias() { 
   const [loading, setLoading] = React.useState(true);
 
   useEffect(() => {
@@ -20,28 +22,88 @@ export default function Noticias() {
     return <Loader />;
   }
 
+  const cardData = {
+    photo: "/rio-redirecionamento.jpg",
+    info: "Descrição da imagem",
+    title: "Título do Card",
+    description: "Descrição do Card",
+    link: "/noticias",
+  };
+
   return (
     <div className={styles.Container}>
-    <Header bannerTitle={"BORA VIAJAR"} />
+      <Header bannerTitle={"BORA VIAJAR"} />
 
-    <div className={styles.bannerContainer}>
+      <div className={styles.bannerContainer}>
         <div className={styles.textContainer}>
-        <h2 className={styles.titleBanner}> Planeje os melhores momentos</h2>
-        <h1 className={styles.subtitleBanner}>Bora viajar!</h1>
+          <h2 className={styles.titleBanner}> Planeje os melhores momentos</h2>
+          <h1 className={styles.subtitleBanner}>Bora viajar!</h1>
         </div>
         <button className={styles.buttonBanner}>Comece Já!</button>
-    </div>
+      </div>
 
       <div className={styles.destinoSection}>
-        <div className={styles.DestinoContainer}>
-            <div className={styles.ContentContainer}>
-            <h1 className={styles.Title}>Conheça os melhores destinos</h1>
-            <p className={styles.Text}>Confira abaixo os melhores lugares para a sua próxima viagem!</p>
-            </div>
+        <div className={styles.ContentContainer}>
+          <h1 className={styles.Title}>Conheça os melhores destinos</h1>
+          <p className={styles.Text}>
+            Confira abaixo os melhores lugares para a sua próxima viagem!
+          </p>
         </div>
+
         <div className={styles.destinoCard}>
 
+            <NoticiaCard 
+            photo={cardData.photo}
+            info={cardData.info}
+            title={cardData.title}
+            description={cardData.description}
+             />
+            <NoticiaCard 
+            photo={cardData.photo}
+            info={cardData.info}
+            title={cardData.title}
+            description={cardData.description}
+             />
+            <NoticiaCard 
+            photo={cardData.photo}
+            info={cardData.info}
+            title={cardData.title}
+            description={cardData.description}
+             />
+            <NoticiaCard 
+            photo={cardData.photo}
+            info={cardData.info}
+            title={cardData.title}
+            description={cardData.description}
+             />
+            <NoticiaCard 
+            photo={cardData.photo}
+            info={cardData.info}
+            title={cardData.title}
+            description={cardData.description}
+             />
+            <NoticiaCard 
+            photo={cardData.photo}
+            info={cardData.info}
+            title={cardData.title}
+            description={cardData.description}
+             />
+            <NoticiaCard 
+            photo={cardData.photo}
+            info={cardData.info}
+            title={cardData.title}
+            description={cardData.description}
+             />
+            <NoticiaCard 
+            photo={cardData.photo}
+            info={cardData.info}
+            title={cardData.title}
+            description={cardData.description}
+             />
+
+           
         </div>
+
       </div>
     </div>
   );
