b00392ed1e7e54130fd79a78ebf5a226977e98bc
flavia <flavinharamendes@gmail.com>
2025-05-13 11:25:31 -0300

    fix: links

--- app/feed/page.jsx
+++ app/feed/page.jsx
@@ -64,6 +64,7 @@ export default function HomePage() {
             textTransform: "uppercase",
             letterSpacing: "1px",
             boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
+            
           }}
           >
             Conheça equipe!
--- components/Header.jsx
+++ components/Header.jsx
@@ -23,10 +23,10 @@ const Header = () => {
       </div>  
 
 <nav className={styles.navBar}>
-  <Link href="/home" className={styles.navLink}>Noticias</Link>
+  <Link href="/noticias" className={styles.navLink}>Noticias</Link>
   <Link href="/home" className={styles.navLink}>Mais procurados</Link>
-  <Link href="/home" className={styles.navLink}>Sobre Nós</Link>
-  <Link href="/home" className={styles.navLink}>Feed</Link>
+  <Link href="/sobre-nos" className={styles.navLink}>Sobre Nós</Link>
+  <Link href="/feed" className={styles.navLink}>Feed</Link>
   <Link href="/home" className={styles.navLink}>Contato</Link>
   <Link href="/home" className={styles.navLink}>Home</Link>
       </nav>
