8e04e808d1dc177e5fa9ebde7b9f18100d3cf21d
Amanda Gomes Mechi <amandamechi327@gmail.com>
2025-05-15 14:43:29 -0300

    Feat: Create Form for Cadastro

--- app/contato/page.jsx
+++ app/contato/page.jsx
@@ -127,17 +127,16 @@ const Contato = () => {
           </div>
         </div>
 
-
         <div className={styles.mapBox}>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2597.368392540495!2d-47.010399387728135!3d-22.97843564297745!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8cd9c133b52f5%3A0xbe859c603dcb641b!2sEscola%20SENAI%20de%20Valinhos!5e0!3m2!1spt-BR!2sbr!4v1747158652210!5m2!1spt-BR!2sbr"
-            width="300%"
-            height="200"
-            style={{ border: 0 }}
-            allowFullScreen
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2597.368392540495!2d-47.010399387728135!3d-22.97843564297745!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8cd9c133b52f5%3A0xbe859c603dcb641b!2sEscola%20SENAI%20de%20Valinhos!5e0!3m2!1spt-BR!2sbr!4v1747158652210!5m2!1spt-BR!2sbr"
+                  width="300%"
+                  height="200"
+                  style={{ border: 0 }}
+                  allowFullScreen
+                  loading="lazy"
+                  referrerPolicy="no-referrer-when-downgrade"
+                ></iframe>
 
         </div>
       </div>
