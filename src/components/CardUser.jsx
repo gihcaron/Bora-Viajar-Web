37719bff91d752971fd22bccd0138efcd900da56
Giovanna CAron <giovanna.c.barros@aluno.senai.br>
2025-06-03 16:18:25 -0300

    Feat: Create Users page

--- src/components/CardUser.jsx
+++ src/components/CardUser.jsx
@@ -3,38 +3,45 @@
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
-import {Card } from "antd";
+import { Card } from "antd";
 import axios from "axios";
 import styles from "../styles/CardUser.module.css"
 
-const UserCard = ({photo, name, city, state}) => {
-    
+const UserCard = ({ photo,  type_user, name, city, state }) => {
+
     const [seguindo, setSeguindo] = useState(false);
 
     return (
         <div className={styles.cardContainer}>
-       
-        <Card className={styles.card}>
-            { usuario && (<p className={styles.typeUser}> Usuário</p>) }
-            { guiaTuristico && (<p className={styles.typeUser}> Guia</p>) } */}            <Image 
-            className={styles.userImage}
-            src={photo}
-            alt={name}
-            width={150}
-            height={150}
-            priority={true}
-             />
-            
-            <h1 className={styles.userName}>{name}</h1>
-            <div className={styles.locationContainer}>
-            <p className={styles.city}>{city}</p>
-            <p className={styles.state}>{state}</p>
-            </div>
-            <p className={styles.typeUser}
-            onClick={() => setSeguindo((prev) => !prev)}> 
-            {seguindo ? "Seguindo" : "Seguir"}
-            </p>
-        </Card>
+
+            <Card className={styles.card}>
+
+                { type_user === "usuário" && (
+                    <p className={styles.UserAndGuia}>Usuário</p>
+                )}
+
+                { type_user === "guia turístico" && (
+                    <button className={styles.UserAndGuia}>Guia</button>
+                )}
+                <Image
+                    className={styles.userImage}
+                    src={`http://localhost:3000/uploads/` + photo}
+                    alt={name}
+                    width={150}
+                    height={150}
+                    priority={true}
+                />
+
+                <h1 className={styles.userName}>{name}</h1>
+                <div className={styles.locationContainer}>
+                    <p className={styles.city}>{city}</p>
+                    <p className={styles.state}>{state}</p>
+                </div>
+                <p className={styles.typeUser}
+                    onClick={() => setSeguindo((prev) => !prev)}>
+                    {seguindo ? "Seguindo" : "Seguir"}
+                </p>
+            </Card>
 
         </div>
     );
